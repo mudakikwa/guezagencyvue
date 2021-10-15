76b12d2acdcd341dbb590ebf482ac87e2f8a165d
mudakikwa <mudakikwastory@gmail.com>
2021-10-15 14:30:30 +0200

    latest changes

---
+++ src/components/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home"></div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    title: String,
+    mission: String,
+    missionDetails: String,
+  },
+};
+</script>
+<style lang="scss">
+@import "./index.scss";
+</style>
